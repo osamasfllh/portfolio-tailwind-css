4a26fdc277b5fa3c9d25ba69dbcdd8be864637d2
Osama Saefulloh <saefullohosama173@gmail.com>
2022-07-12 20:43:03 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '100px',
+    },
+    extend: {
+      colors: {
+        primary: '#8b5cf6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
